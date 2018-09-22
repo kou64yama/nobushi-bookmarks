35880cefad32b0b697335905c6281b811733f66f
YAMADA Koji <kou64yama@gmail.com>
2018-09-22 19:59:35 +0900

    :sparkles: Add login/logout features

---
+++ src/routes/LoginPage/LoginPage.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { mapState } from 'vuex';
+
+@Component({
+  computed: {
+    ...mapState('auth', ['error', 'loading']),
+  },
+})
+export default class LoginPage extends Vue {
+  public email = '';
+  public password = '';
+
+  public mounted() {
+    this.$store.dispatch('document/setTitle', 'Log in');
+  }
+
+  public signIn() {
+    this.$store.dispatch('auth/signInWithEmailAndPassword', {
+      email: this.email,
+      password: this.password,
+    });
+  }
+}
